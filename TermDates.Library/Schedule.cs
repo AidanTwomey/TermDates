d1e11bb6b6c4d218b92e2273903034594fea3e20
Aidan Twomey <aidantwomey@Aidans-MacBook-Pro.local>
2017-12-22 23:42:25 +0000

    fiddling with duration

--- TermDates.Library/Schedule.cs
+++ TermDates.Library/Schedule.cs
@@ -4,8 +4,11 @@ namespace aidantwomey.src.Azure.Functions.TermDates.TermDates.Library
     using System.Collections.Generic;
 
     public class Lesson{
+
+        private TimeSpan _duration;
+
         public DateTime Start { get;set;}
-        public TimeSpan Duration { get; set;}
+        public TimeSpan Duration { get { return _duration - TimeSpan.FromMinutes(5);} set { _duration = value;} }
         public bool Shared { get;set;}
     }
 
