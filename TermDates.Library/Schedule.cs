3342fe5b5674e0b379e2992045bb8958db6d5077
Aidan Twomey <aidantwomey@Aidans-MacBook-Pro.local>
2017-12-22 23:46:04 +0000

    reverting

--- TermDates.Library/Schedule.cs
+++ TermDates.Library/Schedule.cs
@@ -4,6 +4,7 @@ namespace aidantwomey.src.Azure.Functions.TermDates.TermDates.Library
     using System.Collections.Generic;
 
     public class Lesson{
+
         public DateTime Start { get;set;}
         public TimeSpan Duration { get; set;}
         public bool Shared { get;set;}
