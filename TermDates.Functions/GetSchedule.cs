a6ec7a56488f1cb4e94fff915d731082d0344ddf
Aidan Twomey <aidan.twomey@runpath.com>
2019-04-10 16:47:07 +0100

    cleanup

--- TermDates.Functions/GetSchedule.cs
+++ TermDates.Functions/GetSchedule.cs
@@ -23,7 +23,6 @@ namespace TermDates.Functions
             [OrchestrationClient] DurableOrchestrationClient starter,
             ILogger log)
         {
-            // Function input comes from the request content.
             var eventData = await req.Content.ReadAsAsync<ScheduleRequest>();
             string instanceId = await starter.StartNewAsync("ChargeForTerm", eventData);
 
@@ -31,36 +30,6 @@ namespace TermDates.Functions
 
             return starter.CreateCheckStatusResponse(req, instanceId);
         }
-
-        //[FunctionName("ChargeForTermDurableFunctionTrigger")]
-        //public static async Task<IActionResult> RunFromTimer(
-        //    [HttpTrigger(AuthorizationLevel.Function, "POST", Route = "schedule")] HttpRequest req,
-        //    [OrchestrationClient] DurableOrchestrationClient starter,
-        //    [Inject] ILogger log
-        //    )
-        //{
-        //    //loggingContext.CorrelationId = Guid.NewGuid().ToString();
-
-        //    //logger.Info($"Timer trigger function executed at: {DateTime.Now}", loggingContext);
-
-        //    //await starter.StartNewAsync(FunctionNames.MercerIngestOrchestrationFunction, null);
-
-        //    //logger.Info($"Finished MercerOrchestrationFunction", loggingContext);
-
-        //    log.LogInformation("C# HTTP trigger function processed a request.");
-
-        //    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        //    var request = JsonConvert.DeserializeObject<ScheduleRequest>(requestBody);
-
-        //    //var schedule = scheduler.Generate(term, lessonDefinitions);
-
-        //    var instance = await starter.StartNewAsync("ChargeForTerm", request);
-
-        //    await starter.GetStatusAsync(instance);
-
-        //    starter.
-
-        //}
 
         [FunctionName("ChargeForTerm")]
         public static async Task<Schedule> ChargeForTermFunctionAsync([OrchestrationTrigger] DurableOrchestrationContext context)
@@ -92,38 +61,7 @@ namespace TermDates.Functions
             [ActivityTrigger] (Term term, IEnumerable<LessonDefinition> lessons) termLessons,
             [Inject] Scheduler scheduler)
         {
-            //var s = await Task.Run( () => scheduler.Generate(termLessons.term, termLessons.lessons) );
-            //return s;
-
-            //return new Schedule() { Lessons = new List<Lesson>() { new Lesson() { Day = DayOfWeek.Monday, Duration = new TimeSpan(0, 30, 0), Shared = false, WeeksPerLesson = 1 } } };
             return scheduler.Generate(termLessons.term, termLessons.lessons);
-        }
-
-        //[FunctionName("GetSchedule")]
-        //public static async Task<Schedule> Run(
-        //    [HttpTrigger(AuthorizationLevel.Function, "post", Route = "schedule")] HttpRequest req,
-        //    [Inject] Scheduler scheduler,
-        //    ILogger log)
-        //{
-        //    log.LogInformation("C# HTTP trigger function processed a request.");
-                        
-        //    string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        //    var request = JsonConvert.DeserializeObject<ScheduleRequest>(requestBody);
-
-        //    var term = new Term()
-        //    {
-        //        Start = request.TermStart,
-        //        End = request.TermEnd
-        //    };
-
-        //    var lessonDefinitions = request.Lessons.Select(
-        //        l => new LessonDefinition(l.LessonDay, l.Duration, l.WeeksPerLesson));
-
-        //    log.LogInformation(term.Start.ToLongDateString());
-
-        //    var schedule = scheduler.Generate(term, lessonDefinitions);
-
-        //    return new OkObjectResult(JsonConvert.SerializeObject(schedule));
-        //}
+        }        
     }
 }
